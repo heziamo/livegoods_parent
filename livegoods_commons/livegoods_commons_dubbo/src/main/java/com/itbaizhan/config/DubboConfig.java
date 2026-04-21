6e30d023de7e9f293904fb1ee02ba11796f96ab2
heziamo <2784318091@qq.com>
2026-04-21 18:55:06 +0800

    pom1

---
+++ livegoods_commons/livegoods_commons_dubbo/src/main/java/com/itbaizhan/config/DubboConfig.java
@@ -0,0 +1,11 @@
+package com.itbaizhan.config;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDubbo
+public class DubboConfig {
+
+}
+
